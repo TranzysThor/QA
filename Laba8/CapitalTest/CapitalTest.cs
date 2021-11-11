b59512f9917cd617dd45ad804d7a060aba76b384
Maxim Akulevich <50240613+TranzysThor@users.noreply.github.com>
2021-11-12 00:39:03 +0300

    snapback to reality

--- Laba8/CapitalTest/CapitalTest.cs
+++ Laba8/CapitalTest/CapitalTest.cs
@@ -1,7 +1,6 @@
 using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.DevTools.V85.LayerTree;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
@@ -50,6 +49,8 @@ namespace CapitalTest
         [Test]
         public void AddMarketToFavoriteTest()
         {
+            _webDriver.Manage().Window.Maximize();
+
             _webDriverWait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
 
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -60,15 +61,13 @@ namespace CapitalTest
 
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-            var addToFavoriteButton = _webDriver.FindElements(By.XPath("//div[@class='col favorites']"))[0];
+            var addToFavoriteButton = _webDriver.FindElements(By.XPath("//div[@class='col favorites']//add-instrument-to-watchlist-button"))[0];
             addToFavoriteButton.Click();
             
             var addToFavoritesConfirmationButton =
                 _webDriver.FindElement(By.XPath("//div[@class='popover-add-watchlist__footer']//button[@class='button-main button-main--medium']"));
             addToFavoritesConfirmationButton.Click();
             
-            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
             var favoriteMarketsButton = _webDriver.FindElements(By.TagName("i"))[1];
             favoriteMarketsButton.Click();
             favoriteMarketsButton.Click();
@@ -77,6 +76,7 @@ namespace CapitalTest
 
             Assert.AreEqual(ExpectedFavoritedMarketName, actualFavoritedMarketName);
         }
+        
         [TearDown]
         public void TearDown()
         {
