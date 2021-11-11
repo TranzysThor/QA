7192e291bf1c89271a33ee0ddfa0a11f39d96ee0
Maxim Akulevich <50240613+TranzysThor@users.noreply.github.com>
2021-11-11 22:07:47 +0300

    some changes

--- Laba8/CapitalTest/CapitalTest.cs
+++ Laba8/CapitalTest/CapitalTest.cs
@@ -58,6 +58,8 @@ namespace CapitalTest
             mostTradedMarketsButton.Click();
 
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _webDriverWait.Until(ExpectedConditions.ElementToBeClickable(
+                By.XPath("//div[@class='col favorites']//add-instrument-to-watchlist-button//i")));
 
             var addToFavoriteButton = _webDriver.FindElements(By.XPath("//div[@class='col favorites']//add-instrument-to-watchlist-button//i"))[0];
             addToFavoriteButton.Click();
