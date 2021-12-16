e3d71ddf2dad5f5574bf648c0092287d7846e3da
TranzysThor <maxim.akulevich@gmail.com>
2021-12-16 21:24:48 +0300

    add laba9 fixed

---
+++ capitalpageobjectmodel/Page/PageFactoryBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using SeleniumExtras.WaitHelpers;
+
+namespace CapitalPageObjectModel.Page
+{
+    public class PageFactoryBase
+    {
+        protected readonly IWebDriver Driver;
+        protected WebDriverWait Wait;
+
+        protected PageFactoryBase(IWebDriver driver)
+        {
+            Driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public IWebElement FindElementToBeClickable(By elementLocator, double waitTime)
+        {
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(waitTime));
+            return Wait.Until(ExpectedConditions.ElementToBeClickable(elementLocator));
+        }
+    }
+}
